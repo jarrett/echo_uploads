602316db00c51854358fdf66d8e193bbca7eba29
Jarrett Colby <jarrettcolby@gmail.com>
2014-07-09 15:20:07 -0400

    Started refactoring to support the :multiple option

--- example/app/models/widget.rb
+++ example/app/models/widget.rb
@@ -5,20 +5,29 @@ class Widget < ActiveRecord::Base
   
   echo_upload :manual
   echo_upload :thumbnail
-  echo_upload :photo, map: :resize_photo
+  echo_upload :photo, map: :resize_photo, multiple: true
   
   validates :name, presence: true, length: {maximum: 255}
   validates :thumbnail, upload: {presence: true, max_size: 1.5.kilobytes, extension: ['.jpg', '.png']}
   
   private
   
-  def resize_photo(in_file, out_file_path)
+  def resize_photo(in_file, mapper)
     ImageScience.with_image(in_file.path) do |in_image|
       in_image.cropped_thumbnail(200) do |out_image|
         # ImageScience won't let you specify an image format as a param of the #save
         # method. It guesses based on the file extension. So we have to do a little dance.
-        out_image.save(out_file_path + '.png')
-        FileUtils.mv(out_file_path + '.png', out_file_path)
+        mapper.write do |out_file_path|
+          out_image.save(out_file_path + '.png')
+          FileUtils.mv(out_file_path + '.png', out_file_path)
+        end
+      end
+      
+      in_image.cropped_thumbnail(300) do |out_image|
+        mapper.write do |out_file_path|
+          out_image.save(out_file_path + '.png')
+          FileUtils.mv(out_file_path + '.png', out_file_path)
+        end
       end
     end
   end
